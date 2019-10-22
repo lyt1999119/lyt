ea77eebcc59f238e801c8284d495a2a10013ace0
lyt1999119 <56864374+lyt1999119@users.noreply.github.com>
2019-10-22 17:19:29 +0800

    Update calc_inputs.py

--- calc_inputs.py
+++ calc_inputs.py
@@ -1,19 +1,18 @@
 import math
 
 def calc(a,b,c):
+ if a != 0:
     key=b**2-4*a*c
-    if key>0:
-        x1=(-b+math.sqrt(key))/2*a
-        x2=(-b-math.sqrt(key))/2*a
-    if key==0:
+    if key<0:
+       print("无解")
+    elif key==0:
         x1=-b/2*a
         x2=x1
-    if key<0:
-        print('方程无解'）
-        return(None,None)
-    return(x1,x2)
-print(calc(a,b,c))
-
+        print("唯一根",x1)
+    else:
+        x1=(-b+math.sqrt(key))/2*a
+        x2=(-b-math.sqrt(key))/2*a
+        print("x1=",x1,"\t","x2=",x2)
 
     
         
